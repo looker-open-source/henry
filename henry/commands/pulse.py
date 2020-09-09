67e6842e320f7fa80b42b296e3d1a1d1469db728
Joseph Axisa <jax@looker.com>
2020-09-09 11:12:55 +0000

    Update reserved connection names (#33)
    * added looker__ilooker as a reserved connection name
    
    * bumped version to 0.2.3

--- henry/commands/pulse.py
+++ henry/commands/pulse.py
@@ -28,7 +28,7 @@ class Pulse(fetcher.Fetcher):
         """
         print("\bTest 1/6: Checking connections")
 
-        reserved_names = ["looker__internal__analytics", "looker"]
+        reserved_names = ["looker__internal__analytics", "looker", "looker__ilooker"]
         db_connections: Sequence[models.DBConnection] = list(
             filter(lambda c: c.name not in reserved_names, self.sdk.all_connections())
         )
